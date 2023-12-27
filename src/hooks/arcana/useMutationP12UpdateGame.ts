600c11a88e9be99a7d78bbdcd6ce670962998ebf
C-Green-Code <shallowgreenjob@gmail.com>
2023-12-27 15:24:55 +0800

    feat: news & edit sendEvent

--- src/hooks/arcana/useMutationP12UpdateGame.ts
+++ src/hooks/arcana/useMutationP12UpdateGame.ts
@@ -1,4 +1,5 @@
 import { UpdateP12GameParams, updateP12GameInfo } from '@/api';
+import { sendEvent } from '@/utils';
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 
@@ -6,10 +7,12 @@ export const useMutationP12UpdateGame = () => {
   return useMutation({
     mutationFn: (data: UpdateP12GameParams) => updateP12GameInfo(data),
     onSuccess: ({ data }) => {
+      sendEvent('ed_edit_save', '编辑游戏：保存', { action: 1, result: 1 });
       toast.success('Update the work successfully.');
       return data;
     },
     onError: () => {
+      sendEvent('ed_edit_save', '编辑游戏：保存', { action: 1, result: 0 });
       toast.error('Update the work failed. Please try again.');
     },
   });

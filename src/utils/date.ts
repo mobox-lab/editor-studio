4d386ed86f6e41fc57ae495f7f9d4e1b776d0cea
C-Green-Code <shallowgreenjob@gmail.com>
2024-01-03 17:42:50 +0800

    fix: neo dateStr

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,10 @@
 import dayjs, { Dayjs } from 'dayjs';
 
 export function computeTimeDifference(targetTime: Dayjs, now: Dayjs = dayjs()) {
+  const diffInDays = targetTime.diff(now, 'day');
+  const diffInHours = targetTime.diff(now, 'hour');
+  const diffInMinutes = targetTime.diff(now, 'minute');
   const diffInSeconds = targetTime.diff(now, 'second');
-
-  const diffInDays = Math.floor(diffInSeconds / (3600 * 24));
-  const diffInHours = Math.floor(diffInSeconds / 3600);
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
 
   if (diffInDays > 0) {
     return { value: diffInDays, str: 'Days' };

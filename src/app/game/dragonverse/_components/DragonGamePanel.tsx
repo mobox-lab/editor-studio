f116cda0ec14159efed722f2c3c1ecdcebcae7e1
C-Green-Code <shallowgreenjob@gmail.com>
2024-01-03 16:36:21 +0800

    feat: add dragon test

--- src/app/game/dragonverse/_components/DragonGamePanel.tsx
+++ src/app/game/dragonverse/_components/DragonGamePanel.tsx
@@ -1,12 +1,14 @@
 'use client';
-import KeySvg from '@/../public/svg/key.svg?component';
-import { GparkGameDetail } from '@/api';
-import StyledButton from '@/components/ui/button/StyledButton';
-import Tag from '@/components/ui/tag';
-import { openExternalLink } from '@/utils';
-import { clsx } from 'clsx';
 import { useEffect, useMemo, useState } from 'react';
+import { clsx } from 'clsx';
+import Tag from '@/components/ui/tag';
+import { GparkGameDetail } from '@/api';
 import DragonBorder from './DragonBorder';
+import { openExternalLink } from '@/utils';
+import { useRouter } from 'next/navigation';
+import KeySvg from '@/../public/svg/key.svg?component';
+import { launcherConfig } from '@/constants/launcher-config';
+import StyledButton from '@/components/ui/button/StyledButton';
 
 type DragonGamePanelProps = {
   data?: GparkGameDetail;
@@ -15,7 +17,9 @@ type DragonGamePanelProps = {
 };
 
 export default function DragonGamePanel({ data, isLoading, handleRunningGame }: DragonGamePanelProps) {
+  const router = useRouter();
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [clickCount, setClickCouent] = useState(0);
   const imageList = useMemo(() => data?.images.map((item) => item.url) ?? [], [data?.images]);
 
   useEffect(() => {
@@ -25,6 +29,12 @@ export default function DragonGamePanel({ data, isLoading, handleRunningGame }: 
 
     return () => clearInterval(timer);
   }, [imageList.length]);
+
+  useEffect(() => {
+    if (clickCount === 8) {
+      router.push('/game/' + launcherConfig.TestDragonVerseGameId);
+    }
+  }, [clickCount, router]);
 
   return (
     <div className="grid grid-cols-2 border border-gray-400 bg-gray-550/10">
@@ -54,24 +64,19 @@ export default function DragonGamePanel({ data, isLoading, handleRunningGame }: 
       <div className="relative p-3">
         <DragonBorder className="inset-2 -z-10" />
         <div className="relative px-6 pt-6">
-          <img draggable={false} src="/img/gpark/dragon-title.webp" alt="DragonVerse" className="h-15" />
+          <img
+            onClick={() => setClickCouent((c) => c + 1)}
+            draggable={false}
+            src="/img/gpark/dragon-title.webp"
+            alt="DragonVerse"
+            className="h-15"
+          />
           <div className="mt-3 flex h-5 items-center gap-1.5">
             <Tag>Mobox</Tag>
             <Tag>Vista</Tag>
           </div>
           <div className="mt-6 h-20 overflow-y-scroll whitespace-pre-line text-xs/5">{data?.description}</div>
           <img src="/img/gpark/mobox.webp" alt="mobox-icon" className="mt-6 h-4" />
-          {/* <div className="absolute bottom-0 flex h-11 items-center gap-2">
-            <div className="relative h-10.5 w-10.5 overflow-hidden rounded-full">
-              {author && <img className="h-full w-full object-cover" loading="lazy" src={author.avatar} alt="avatar" />}
-            </div>
-            <div className="">
-              <div className="text-base/5">
-                <span className="font-medium">{author?.name}</span>&nbsp;
-              </div>
-              <div className="mt-0.5 text-xs/5">{author?.introduction}</div>
-            </div>
-          </div> */}
         </div>
         <div className="mb-3 mt-17 flex gap-3 px-6">
           <StyledButton
--- src/constants/launcher-config.ts
+++ src/constants/launcher-config.ts
@@ -5,25 +5,29 @@ type LauncherConfig = {
   roomurl: string;
   avatarGameId: string;
   dragonVerseGameId: string;
+  TestDragonVerseGameId: string;
 };
 const launcherConfigMap: Record<string, LauncherConfig> = {
   'gpark-test-oversea': {
     environment: 'gpark-test-oversea',
     roomurl: 'ws://gate-api.metaworld.fun:20011',
     avatarGameId: 'h6ZjaE8nZrQWaJ9RDNJy',
-    dragonVerseGameId: 'D5LIrBLH5XJ2nhYqrk4M',
+    dragonVerseGameId: 'cXYQHB995aEXFIZqoc0o',
+    TestDragonVerseGameId: 'D5LIrBLH5XJ2nhYqrk4M',
   },
   'gpark-pre-oversea': {
     environment: 'gpark-pre-oversea',
     roomurl: 'ws://pre-mw-gate-api.gpark.io:20011',
     avatarGameId: '6KEKRY9qMe1Rf85Oa5wq',
     dragonVerseGameId: 'ODA0bxo4W1amzL1PZQTR',
+    TestDragonVerseGameId: 'ODA0bxo4W1amzL1PZQTR',
   },
   'gpark-online-oversea': {
     environment: 'gpark-online-oversea',
     roomurl: 'ws://mw-gate-api.gpark.io:20011',
     avatarGameId: '6KEKRY9qMe1Rf85Oa5wq',
     dragonVerseGameId: 'ODA0bxo4W1amzL1PZQTR',
+    TestDragonVerseGameId: 'ODA0bxo4W1amzL1PZQTR',
   },
 };
 

4ae241ca4c2d13fd727596bb123a1075ac7ed8ee
C-Green-Code <shallowgreenjob@gmail.com>
2024-01-03 13:56:21 +0800

    feat: neo date str & vote total

--- src/utils/shorten.ts
+++ src/utils/shorten.ts
@@ -61,3 +61,14 @@ export function shortenNumber(value: number, digits: number = 4, startTruncateVa
   }
   return (value / t.num).toPrecision(digits) + t.type;
 }
+
+export function shortenSnapStr(str: string, key?: any): string {
+  if (!str) return str;
+  let limit;
+  if (typeof key === 'number') limit = key;
+  if (key === 'symbol') limit = 6;
+  if (key === 'name') limit = 64;
+  if (key === 'choice') limit = 12;
+  if (limit) return str.length > limit ? `${str.slice(0, limit).trim()}...` : str;
+  return shortenAddress(str);
+}

4431f281f0c879c46ed13d066345f756138c2eb8
C-Green-Code <shallowgreenjob@gmail.com>
2024-01-31 14:01:00 +0800

    fix: mbox symbol

--- src/api/mobox/index.ts
+++ src/api/mobox/index.ts
@@ -4,7 +4,7 @@ import { STORAGE_KEY } from "@/constants/storage";
 
 export const getMoboxAccessToken = () => request.post<any, Response<{ token: string }>>('/oauth/p12', { ptoken: window.localStorage.getItem(STORAGE_KEY.P12_TOKEN), });
 
-export const fetchBoxWallet = () => request.post<any, Response<BoxWallet>>('/user/symbol/balance', { symbol: 'mobox' });
+export const fetchBoxWallet = () => request.post<any, Response<BoxWallet>>('/user/symbol/balance', { symbol: 'mbox' });
 
 export const fetchDragonUserBag = (addr?: string) =>
   request.get<any, Response<DragonBag>>('https://nft-api.mobox.io/nft/dragon/user/bag', { params: { addr } });

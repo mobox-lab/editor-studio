b2d37dbc40b557745c62eb9bc43a8e94d92b7cf8
C-Green-Code <shallowgreenjob@gmail.com>
2024-12-12 11:47:41 +0800

    chore: add s8 gamecode

--- src/constants/games.ts
+++ src/constants/games.ts
@@ -19,7 +19,7 @@ enum GameEnv {
 const DV_CONFIG_MAP: Record<string, DvConfig[]> = {
   [GameEnv.Test]: [{ name: 'Merlin Beta', code: 'D5LIrBLH5XJ2nhYqrk4M' }],
   [GameEnv.Online]: [
-    { name: 'Merlin Release', code: 'mi3ELtZR2ucsG94d88Px' },
+    { name: 'Merlin Release', code: '1pEkIYivlf7uG0e6nnEh' },
     { name: 'Bsc Release', code: 'UcJsqnsne3DvCnZxafFp' },
     { name: 'Merlin Beta S6', code: '42qwxtWuRAiAYjq4YEfF' },
     { name: 'Merlin Beta 038', code: 'ak0XtpACHGf9kMSgfKjR' },

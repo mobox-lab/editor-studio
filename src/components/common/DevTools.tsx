1f29c9d62401d516bbf042020df9bf9b8c31a649
C-Green-Code <shallowgreenjob@gmail.com>
2025-02-20 15:58:27 +0800

    chore: open s11 game

--- src/components/common/DevTools.tsx
+++ src/components/common/DevTools.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 export default function DevTools() {
   return (
     <>
-      <Script id="eruda" strategy="beforeInteractive" src="https://cdn.bootcdn.net/ajax/libs/eruda/3.0.1/eruda.js" />
+      <Script id="eruda" strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/eruda/3.4.1/eruda.min.js" />
       <Script id="eruda-init" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: 'window.eruda.init()' }} />
     </>
   );

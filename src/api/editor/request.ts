4adea474176d48522b8d1a0ef38018a973d2019f
joji-mb <545633740@qq.com>
2025-04-18 21:43:55 +0800

    test: 🧪 add memory leak tests

--- src/api/editor/request.ts
+++ src/api/editor/request.ts
@@ -39,3 +39,9 @@ instance.interceptors.response.use(
 );
 
 export default instance;
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};

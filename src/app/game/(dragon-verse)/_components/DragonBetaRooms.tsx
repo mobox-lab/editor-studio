6a7b01c62c5ed2098ecbb7b31cac79a973881ce1
C-Green-Code <shallowgreenjob@gmail.com>
2025-04-19 18:35:27 +0800

    docs: 📝 add deployment checklist

--- src/app/game/(dragon-verse)/_components/DragonBetaRooms.tsx
+++ src/app/game/(dragon-verse)/_components/DragonBetaRooms.tsx
@@ -59,3 +59,9 @@ const DragonBetaRooms = forwardRef(function DragonBetaRooms(
 });
 
 export default DragonBetaRooms;
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};

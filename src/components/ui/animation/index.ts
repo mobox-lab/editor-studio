fd50229e3e2073252ffebfe7d2a7d20f7b349f85
joji-mb <545633740@qq.com>
2025-05-01 20:58:58 +0800

    test: 🧪 add component testing

--- src/components/ui/animation/index.ts
+++ src/components/ui/animation/index.ts
@@ -1,1 +1,7 @@
 export * from './BadgeBackground';
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};

9bd5a2d4ef32fca241561664d956c967101fb95c
C-Green-Code <shallowgreenjob@gmail.com>
2025-05-03 09:48:32 +0800

    perf: ⚡ reduce bundle size

--- src/atoms/gpark/category.ts
+++ src/atoms/gpark/category.ts
@@ -2,3 +2,9 @@ import { atom } from 'jotai';
 import { GparkCardItem } from '@/api';
 
 export const categoriesCardAtom = atom<GparkCardItem[]>([]);
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};

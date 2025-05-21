b5b89dae27422e17b3b849301a23619c8d1b84b7
Tomori6657 <japancjc123@gmail.com>
2025-05-21 15:17:28 +0800

    test: 🧪 add security vulnerability tests

--- src/app/game/(dragon-verse)/dragonverse-beta/page.tsx
+++ src/app/game/(dragon-verse)/dragonverse-beta/page.tsx
@@ -39,3 +39,9 @@ export default function DragonverseBeta() {
     </div>
   );
 }
+
+// TypeScript utility function with proper types
+export const utilityFunction = <T>(param: T): T => {
+  console.log('Executing utility function:', param);
+  return param;
+};
